5323c6406d8291f3059826c52f349f2e76c5b90f
Petr Ohlídal <petr@ohlidal.cz>
2017-03-13 00:49:38 +0100

    :triangular_ruler: Win32DI: Code cleaned up.

--- source/main/utils/Win32DirectInput.h
+++ source/main/utils/Win32DirectInput.h
@@ -3,28 +3,22 @@
 
 #ifdef _WIN32
 
-struct Win32DIJoyState // DirectInput joystick state
+namespace Win32DI // DirectInput wrapper
 {
-    int pos_x;
-    int pos_y;
-    int pos_z;
-    int rot_x;
-    int rot_y;
-    int rot_z;
-};
+    struct JoyState
+    {
+        int pos_x;
+        int pos_y;
+        int pos_z;
+        int rot_x;
+        int rot_y;
+        int rot_z;
+    };
 
-class Win32DirectInput
-{
-public:
-    bool Init(const char* hwnd_str);
-    void Shutdown();
-    bool Update();
-
-    inline Win32DIJoyState GetJoyState() { return m_joy_state; }
-
-private:
-    unsigned __int64 m_w_handle;
-    Win32DIJoyState  m_joy_state;
+    bool        Init(const char* hwnd_str);
+    void        Shutdown();
+    bool        Update();
+    JoyState*   GetJoyState();
 };
 
 #endif // _WIN32
