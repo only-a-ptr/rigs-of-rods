6ef48624a634cbf172f9da7af5198c8b1b1aeac2
Petr Ohlídal <petr@ohlidal.cz>
2017-12-07 00:33:03 +0100

    Added VisualStudio2015 project + libs

---
+++ source/main/RoRVersion.h
@@ -0,0 +1,27 @@
+/*
+This source file is part of Rigs of Rods
+Copyright 2005-2012 Pierre-Michel Ricordel
+Copyright 2007-2012 Thomas Fischer
+
+For more information, see http://www.rigsofrods.org/
+
+Rigs of Rods is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, as
+published by the Free Software Foundation.
+
+Rigs of Rods is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Rigs of Rods.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define ROR_VERSION_STRING_SHORT "0.4" // This defines the home directory name (MyDocuments/Rigs of Rods ${ROR_VERSION_STRING_SHORT}) !!!
+#define ROR_VERSION_STRING "ELSACO v5"
+#define ROR_BUILD_DATE "d"
+#define ROR_BUILD_TIME "t"
+
