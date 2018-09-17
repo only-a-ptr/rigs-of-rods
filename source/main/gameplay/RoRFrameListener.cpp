fb8bdd0364a59a7229118b3d29b0c1f39840ed63
ulteq <ulteq@web.de>
2018-09-17 12:03:58 +0200

    :bug: Fixes the interactive truck reset stuttering

--- source/main/gameplay/RoRFrameListener.cpp
+++ source/main/gameplay/RoRFrameListener.cpp
@@ -1576,12 +1576,6 @@ void SimController::UpdateSimulation(float dt)
     }
 #endif // USE_OPENAL
 
-    if ((simRUNNING(s) || simEDITOR(s)) && !simPAUSED(s))
-    {
-        m_actor_manager.UpdateAirbrakeInput(dt);
-        m_actor_manager.UpdateActorVisuals(dt, m_player_actor); // update visual - antishaking
-    }
-
     this->UpdateInputEvents(dt);
 
     // CAUTION: 'updateEvents' might have changed 'm_player_actor'
@@ -1621,6 +1615,8 @@ void SimController::UpdateSimulation(float dt)
 
             m_gfx_scene.BufferSimulationData();
 
+            m_actor_manager.UpdateAirbrakeInput(dt);
+            m_actor_manager.UpdateActorVisuals(dt, m_player_actor); // Needs to happen after BufferSimulationData()
             m_actor_manager.UpdateActors(m_player_actor, dt); // *** Start new physics tasks. No reading from Actor N/B beyond this point.
         }
     }
