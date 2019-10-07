20d82cf7a0bab30daed85c415bcda965c278a8b2
Petr Ohlidal <petr@ohlidal.cz>
2019-10-07 12:50:19 +0200

    :wrench: Check modcache for corruption before spawning actor
    If invalid, log details, clear cache, stop simulation and instruct user to rebuild cache.

--- source/main/resources/CacheSystem.h
+++ source/main/resources/CacheSystem.h
@@ -228,6 +228,7 @@ public:
     void                  UnloadActorFromMemory(std::string filename);
     CacheValidityState    EvaluateCacheValidity();
     size_t                Query(CacheQuery& query);
+    void                  ClearCache(); // removes all files from the cache
 
     void LoadResource(CacheEntry& t); //!< Loads the associated resource bundle if not already done.
     bool CheckResourceLoaded(Ogre::String &in_out_filename); //!< Finds + loads the associated resource bundle if not already done.
@@ -259,7 +260,6 @@ private:
     bool ParseKnownFiles(Ogre::String group); // returns true if no known files are found
     void ParseSingleZip(Ogre::String path);
 
-    void ClearCache(); // removes                   all files from the cache
     void PruneCache(); // removes modified (or deleted) files from the cache
 
     void AddFile(Ogre::String group, Ogre::FileInfo f, Ogre::String ext);
