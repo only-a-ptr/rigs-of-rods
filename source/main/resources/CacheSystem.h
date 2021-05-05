8850bd8b52bfdd73279ec6b1661fd008fb738006
Petr Ohlidal <petr@ohlidal.cz>
2021-05-05 03:20:56 +0200

    :jigsaw: Added new cache entry type 'addon' + console UI.
    See AddonReadMe.txt
    
    AddonFileFormat.h/cpp - copypaste of Terrn2FileFormat.h/cpp
    GUI_ConsoleWindow.cpp - DrawAddonSelector() - copypaste of server list UI.

--- source/main/resources/CacheSystem.h
+++ source/main/resources/CacheSystem.h
@@ -236,6 +236,7 @@ private:
 
     void FillTerrainDetailInfo(CacheEntry &entry, Ogre::DataStreamPtr ds, Ogre::String fname);
     void FillTruckDetailInfo(CacheEntry &entry, Ogre::DataStreamPtr ds, Ogre::String fname, Ogre::String group);
+    void FillAddonDetailInfo(CacheEntry &entry, Ogre::DataStreamPtr ds, Ogre::String fname);
 
     void GenerateHashFromFilenames();         //!< For quick detection of added/removed content
 
